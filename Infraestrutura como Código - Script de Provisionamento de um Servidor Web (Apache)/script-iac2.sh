634760bd956fd7a788953de2431fe76f91732f91
mattyonder <mateussantosor5002@gmail.com>
2023-02-20 17:02:34 -0300

    Correções no script

--- Infraestrutura como Código - Script de Provisionamento de um Servidor Web (Apache)/script-iac2.sh
+++ Infraestrutura como Código - Script de Provisionamento de um Servidor Web (Apache)/script-iac2.sh
@@ -6,12 +6,15 @@ apt-get upgrade -y
 
 echo "Instalando Apache 2 e Unzip"
 apt-get install apache2 -y
+sytemctl restart apache2
+#Realizo um reset no serviço do apache2, pois na minha máquina ele não inicia de imediato.
 apt-get install unzip -y
 
-
 echo "Baixando e copiando os arquivos da aplicação..."
-cd /temp
+cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
 cd linux-site-dio-main
 cp -R * /var/www/html/
+
+
