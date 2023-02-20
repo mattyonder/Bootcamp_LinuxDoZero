6c792234bc8508f38971c732f468a60504982535
mattyonder <mateussantosor5002@gmail.com>
2023-02-20 16:04:58 -0300

    Adicionando outras pastas e script do servidor web

---
+++ Infraestrutura como Código - Script de Provisionamento de um Servidor Web (Apache)/script-iac2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Atualizando o servidor"
+apt-get update
+apt-get upgrade -y
+
+echo "Instalando Apache 2 e Unzip"
+apt-get install apache2 -y
+apt-get install unzip -y
+
+
+echo "Baixando e copiando os arquivos da aplicação..."
+cd /temp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
